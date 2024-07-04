0484f11e01f357e1ff923b727a1c6060ee0a513d
omonuj <jonahodoh@gmail.com>
2024-07-04 17:11:37 +0100

    Wire up first terraform config with aws provider and IAM user

--- backup/01-terraform-basics/main.tf
+++ backup/01-terraform-basics/main.tf
@@ -5,4 +5,8 @@ provider "aws" {
 
 # plan - execute 
 resource "aws_s3_bucket" "my_s3_bucket" {
-    bucket = "my-s3-bucket-in28minutes-rangak-002"
+    bucket = "my-s3-bucket-in28minutes-rangak-002"
+ #   versioning {
+ #       enabled = true
+ #   }
+}
