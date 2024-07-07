cb5b89b3e031b56bacb3044992435435e348f041
omonuj <jonahodoh@gmail.com>
2024-07-07 18:48:56 +0100

    Extend IAM user resource for lesson 01

--- 01-terraform-basics/resources.tf
+++ 01-terraform-basics/resources.tf
@@ -5,4 +5,7 @@ provider "aws" {
 # Plan - execute
 resource "aws_s3_bucket" "my_s3_bucket" {
   bucket = "my-s3-bucket-omonuj-001"
-
+
+}
+resource "aws_s3_bucket_versioning" "versioning" {  
+  bucket = aws_s3_bucket.my_s3_bucket.id  
