cb45879721ea6d7673cb1577b7cedc65e822d35e
omonuj <jonahodoh@gmail.com>
2024-07-12 12:17:17 +0100

    Expand variable-driven IAM user config

--- 02-terraform-basics/main.tf
+++ 02-terraform-basics/main.tf
@@ -2,4 +2,9 @@ variable "iam_user_name_prefix" {
   type    = string #any, number, bool, list, map, set, object, tuple
   default = "my_iam_user"
 }
+
+provider "aws" {
+  region  = "us-east-1"
+  //version = "~> 2.46" (No longer necessary)
+}
 
