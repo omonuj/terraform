91730f8bfd1609ad562e9656fc21c2149535b786
omonuj <jonahodoh@gmail.com>
2024-07-15 08:27:06 +0100

    Extend list and set iteration for multiple IAM users

--- 03-list-and-sets/main.tf
+++ 03-list-and-sets/main.tf
@@ -3,4 +3,9 @@ variable "names" {
   #default = ["ranga", "tom", "jane"]
 }
 
-provider "aws" {
+provider "aws" {
+  region  = "us-east-1"
+  //version = "~> 2.46" (No longer necessary)
+}
+
+resource "aws_iam_user" "my_iam_users" {
