bee0f16f316efc3e896529d1f23d9d32d0cd4c21
omonuj <jonahodoh@gmail.com>
2024-07-15 19:05:02 +0100

    Complete list and set iteration for multiple IAM users

--- 03-list-and-sets/main.tf
+++ 03-list-and-sets/main.tf
@@ -8,4 +8,9 @@ provider "aws" {
   //version = "~> 2.46" (No longer necessary)
 }
 
-resource "aws_iam_user" "my_iam_users" {
+resource "aws_iam_user" "my_iam_users" {
+  #count = length(var.names)
+  #name  = var.names[count.index]
+  for_each = toset(var.names)
+  name = each.value
+}
