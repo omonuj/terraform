147041312c4eab7d0edff812b2b89e323dec6a02
omonuj <jonahodoh@gmail.com>
2024-07-16 18:12:42 +0100

    Add resources to map-driven IAM user configuration

--- backup/04-maps/main.tf
+++ backup/04-maps/main.tf
@@ -1,4 +1,8 @@
 variable "users" {
   default = {
     ravs : { country : "Netherlands", department : "ABC" },
-    tom : { country : "US", department : "DEF" },
+    tom : { country : "US", department : "DEF" },
+    jane : { country : "India", department : "XYZ" }
+  }
+}
+
