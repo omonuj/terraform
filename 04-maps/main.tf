b75c5644b6af66f1d36e51865b59c171178092b9
omonuj <jonahodoh@gmail.com>
2024-07-19 13:00:54 +0100

    Iterate on maps and lookups for per-user settings

--- 04-maps/main.tf
+++ 04-maps/main.tf
@@ -1,4 +1,8 @@
 variable "users" {
    default = {
     ravs: {country: "Netherland", department: "Engineering"},
-    tom: {country: "US", department: "Sales"},
+    tom: {country: "US", department: "Sales"},
+    john: {country: "Nigeria", department: "Marketing"},
+   }
+}
+
