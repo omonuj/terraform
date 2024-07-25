177841e5491b0ac33ad1400c4716321141337978
omonuj <jonahodoh@gmail.com>
2024-07-25 14:33:41 +0100

    Layer more onto EC2 instance with security group and key pair

--- backup/05-ec2-instances/main.tf
+++ backup/05-ec2-instances/main.tf
@@ -23,4 +23,11 @@ resource "aws_security_group" "http_server_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
     cidr_blocks = ["0.0.0.0/0"]
