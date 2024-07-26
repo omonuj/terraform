c033cd169adab53933b6a93de5f2612eb9a87e29
omonuj <jonahodoh@gmail.com>
2024-07-26 11:48:42 +0100

    Extend EC2 instance with security group and key pair

--- backup/05-ec2-instances/main.tf
+++ backup/05-ec2-instances/main.tf
@@ -36,4 +36,11 @@ resource "aws_security_group" "http_server_sg" {
   tags = {
     name = "http_server_sg"
   }
-}
+}
+
+resource "aws_instance" "http_server" {
+  #ami                   = "ami-062f7200baf2fa504"
+  ami                    = data.aws_ami.aws_linux_2_latest.id
+  key_name               = "default-ec2"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.http_server_sg.id]
