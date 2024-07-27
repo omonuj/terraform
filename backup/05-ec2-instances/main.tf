b446e8e4c0610d64845488904cd990fdfae52813
omonuj <jonahodoh@gmail.com>
2024-07-27 08:11:12 +0100

    Build out EC2 instance with security group and key pair

--- backup/05-ec2-instances/main.tf
+++ backup/05-ec2-instances/main.tf
@@ -49,4 +49,11 @@ resource "aws_instance" "http_server" {
   subnet_id = data.aws_subnets.default_subnets.ids[0]
 
   connection {
-    type        = "ssh"
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ec2-user"
+    private_key = file(var.aws_key_pair)
+  }
+
+  provisioner "remote-exec" {
+    inline = [
