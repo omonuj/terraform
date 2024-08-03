b54fa7cf8d34c18096f811b5ccc0b3d966c74406
omonuj <jonahodoh@gmail.com>
2024-08-03 15:37:22 +0100

    Add resources to HTTP server EC2 instance with provisioning

--- 05-ec2-instances/main.tf
+++ 05-ec2-instances/main.tf
@@ -24,4 +24,11 @@ resource "aws_default_vpc" "default" {
 
 resource "aws_security_group" "http_server_sg" {
   name   = "http_server_sg"
-  vpc_id = aws_default_vpc.default.id
+  vpc_id = aws_default_vpc.default.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
