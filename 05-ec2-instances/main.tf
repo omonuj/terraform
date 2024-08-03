10043db03867ce5c23f3ec2a632dd97ac9706b6c
omonuj <jonahodoh@gmail.com>
2024-08-03 22:23:16 +0100

    Refine HTTP server EC2 instance with provisioning

--- 05-ec2-instances/main.tf
+++ 05-ec2-instances/main.tf
@@ -38,4 +38,10 @@ resource "aws_security_group" "http_server_sg" {
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  }
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
