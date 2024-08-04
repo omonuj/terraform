3eae9af04fb9eefa26fb17f977e03e881b3ddd57
omonuj <jonahodoh@gmail.com>
2024-08-04 09:23:28 +0100

    Iterate on HTTP server EC2 instance with provisioning

--- 05-ec2-instances/main.tf
+++ 05-ec2-instances/main.tf
@@ -44,4 +44,11 @@ resource "aws_security_group" "http_server_sg" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "http_server_sg"
+  }
+}
+
