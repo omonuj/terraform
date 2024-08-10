f15efcf676907d87566a8c896022c988194c7dac
omonuj <jonahodoh@gmail.com>
2024-08-10 11:56:19 +0100

    Build out EC2 fleet behind a classic elastic load balancer

--- backup/06-ec2-with-elb/main.tf
+++ backup/06-ec2-with-elb/main.tf
@@ -5,4 +5,11 @@ provider "aws" {
 
 resource "aws_default_vpc" "default" {
 
-}
+}
+
+resource "aws_security_group" "http_server_sg" {
+  name = "http_server_sg"
+  //vpc_id = "vpc-c49ff1be"
+  vpc_id = aws_default_vpc.default.id
+
+  ingress {
