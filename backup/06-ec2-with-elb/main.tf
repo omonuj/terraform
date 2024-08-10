2c4839274f0d5f91197696ee87b20e2b61dbc435
omonuj <jonahodoh@gmail.com>
2024-08-10 16:31:07 +0100

    Add resources to EC2 fleet behind a classic elastic load balancer

--- backup/06-ec2-with-elb/main.tf
+++ backup/06-ec2-with-elb/main.tf
@@ -12,4 +12,12 @@ resource "aws_security_group" "http_server_sg" {
   //vpc_id = "vpc-c49ff1be"
   vpc_id = aws_default_vpc.default.id
 
-  ingress {
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
