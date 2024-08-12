c25d456e37f907d8ce16b81c60f02a885f7c7900
omonuj <jonahodoh@gmail.com>
2024-08-12 22:23:37 +0100

    Layer more onto EC2 fleet behind a classic elastic load balancer

--- backup/06-ec2-with-elb/main.tf
+++ backup/06-ec2-with-elb/main.tf
@@ -49,4 +49,11 @@ resource "aws_security_group" "elb_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  egress {
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
