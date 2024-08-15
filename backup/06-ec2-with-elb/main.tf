1d81d628214c8adc2c258daa7eeab2b012129ee2
omonuj <jonahodoh@gmail.com>
2024-08-15 08:01:47 +0100

    Polish EC2 fleet behind a classic elastic load balancer

--- backup/06-ec2-with-elb/main.tf
+++ backup/06-ec2-with-elb/main.tf
@@ -93,4 +93,11 @@ resource "aws_instance" "http_servers" {
     private_key = file(var.aws_key_pair)
   }
 
-  provisioner "remote-exec" {
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum install httpd -y",
+      "sudo service httpd start",
+      "echo Welcome to in28minutes - Virtual Server is at ${self.public_dns} | sudo tee /var/www/html/index.html"
+    ]
+  }
+}
