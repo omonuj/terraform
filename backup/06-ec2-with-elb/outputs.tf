a8a2b545206ab9c0936cf641109f80ec4f7bbc3a
omonuj <jonahodoh@gmail.com>
2024-08-16 11:29:47 +0100

    Wrap up ELB DNS name output

--- backup/06-ec2-with-elb/outputs.tf
+++ backup/06-ec2-with-elb/outputs.tf
@@ -3,4 +3,9 @@ output "aws_security_group_http_server_details" {
 }
 
 output "http_server_public_dns" {
-  value = values(aws_instance.http_servers).*.id
+  value = values(aws_instance.http_servers).*.id
+}
+
+output "elb_public_dns" {
+  value = aws_elb.elb
+}
