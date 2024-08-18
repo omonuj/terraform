d7b691298f2bfd33bc1e33aa18828fd1f2e08483
omonuj <jonahodoh@gmail.com>
2024-08-18 19:08:05 +0100

    Add resources to VPC, subnet and AMI data sources for the ELB setup

--- 06-ec2-with-elb/data-providers.tf
+++ 06-ec2-with-elb/data-providers.tf
@@ -8,4 +8,8 @@ data "aws_ami" "amazon_linux_2" {
   most_recent = true
   owners      = ["amazon"]
 
-  filter {
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
