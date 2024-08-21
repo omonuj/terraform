83634e6c3cf1b29de873930f8257b8fe593059cc
omonuj <jonahodoh@gmail.com>
2024-08-21 11:04:32 +0100

    Flesh out load balanced HTTP servers across multiple subnets

--- 06-ec2-with-elb/main.tf
+++ 06-ec2-with-elb/main.tf
@@ -18,4 +18,11 @@ resource "aws_default_vpc" "default" {
 }
 
 
-###########################################
+###########################################
+# security group
+###########################################
+
+// HTTP Server -> SG
+//SG -> 80 TCP, 22  TCP, CIDR ["0.0.0.0/0"] 
+
+resource "aws_security_group" "elb_sg" {
