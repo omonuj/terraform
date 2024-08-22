8b9017b82b6341ae0f92e55a011f52cc02bd5ae9
omonuj <jonahodoh@gmail.com>
2024-08-22 17:52:05 +0100

    Expand load balanced HTTP servers across multiple subnets

--- 06-ec2-with-elb/main.tf
+++ 06-ec2-with-elb/main.tf
@@ -32,4 +32,11 @@ resource "aws_security_group" "elb_sg" {
   ingress {
     from_port   = 80
     to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
