715a7ae1b3fcff53038aa4e20ab422336d2422db
omonuj <jonahodoh@gmail.com>
2024-08-23 12:02:27 +0100

    Add resources to load balanced HTTP servers across multiple subnets

--- 06-ec2-with-elb/main.tf
+++ 06-ec2-with-elb/main.tf
@@ -46,4 +46,10 @@ resource "aws_security_group" "elb_sg" {
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = "-1"
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+}
+
