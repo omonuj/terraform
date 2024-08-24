b52a94b255bfac139ab05bce1c878504291176ae
omonuj <jonahodoh@gmail.com>
2024-08-24 14:31:11 +0100

    Iterate on load balanced HTTP servers across multiple subnets

--- 06-ec2-with-elb/main.tf
+++ 06-ec2-with-elb/main.tf
@@ -66,4 +66,11 @@ resource "aws_elb" "elb" {
 
 
   listener {
-    instance_port     = 80
+    instance_port     = 80
+    instance_protocol = "HTTP"
+    lb_port           = 80
+    lb_protocol       = "HTTP"
+  }
+
+}
+
