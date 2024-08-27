14ba132b9e2dc2637e7d4a81cb778d49077a074d
omonuj <jonahodoh@gmail.com>
2024-08-27 19:34:12 +0100

    Polish load balancer and instance outputs for lesson 06

--- 06-ec2-with-elb/outputs.tf
+++ 06-ec2-with-elb/outputs.tf
@@ -3,4 +3,9 @@
 # }
 
 output "http_server_public_dns" {
-  value =  values(aws_instance.http_servers).*.id
+  value =  values(aws_instance.http_servers).*.id
+}
+
+output "elb_public_dns" {
+  value = aws_elb.elb
+}
