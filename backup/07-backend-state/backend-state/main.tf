656177cf5d0c22973c0610a648873e5619571b30
omonuj <jonahodoh@gmail.com>
2024-08-29 11:33:35 +0100

    Refine S3 bucket and DynamoDB table to bootstrap remote state

--- backup/07-backend-state/backend-state/main.tf
+++ backup/07-backend-state/backend-state/main.tf
@@ -17,4 +17,10 @@ resource "aws_s3_bucket" "enterprise_backend_state" {
 #
 #    server_side_encryption_configuration {
 #        rule {
-#            apply_server_side_encryption_by_default {
+#            apply_server_side_encryption_by_default {
+#                sse_algorithm = "AES256"
+#            }
+#        }
+#    }
+
+}
