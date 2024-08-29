f4a7f7af5db6296bc39101848b225fa23639825c
omonuj <jonahodoh@gmail.com>
2024-08-29 20:35:42 +0100

    Grow S3 bucket and DynamoDB table to bootstrap remote state

--- backup/07-backend-state/backend-state/main.tf
+++ backup/07-backend-state/backend-state/main.tf
@@ -30,4 +30,10 @@ resource "aws_s3_bucket_versioning" "versioning_example" {
   versioning_configuration {
     status = "Enabled"
   }
-}
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = aws_s3_bucket.enterprise_backend_state.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
