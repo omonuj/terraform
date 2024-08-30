77c2c8cea7f60291ae021b5a268d86930ead9cc0
omonuj <jonahodoh@gmail.com>
2024-08-30 18:46:37 +0100

    Layer more onto S3 bucket and DynamoDB table to bootstrap remote state

--- backup/07-backend-state/backend-state/main.tf
+++ backup/07-backend-state/backend-state/main.tf
@@ -36,4 +36,11 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
   bucket = aws_s3_bucket.enterprise_backend_state.bucket
 
   rule {
-    apply_server_side_encryption_by_default {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "AES256"
+    }
+  }
+}
+
+//Locking - Dynamo DB
+
