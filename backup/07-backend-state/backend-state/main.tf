68831ec32ae77fc9e8d14580b14e9dfbca4d2b80
omonuj <jonahodoh@gmail.com>
2024-08-31 17:02:58 +0100

    Round out S3 bucket and DynamoDB table to bootstrap remote state

--- backup/07-backend-state/backend-state/main.tf
+++ backup/07-backend-state/backend-state/main.tf
@@ -49,4 +49,10 @@ resource "aws_dynamodb_table" "enterprise_backend_lock" {
     billing_mode = "PAY_PER_REQUEST"
 
     hash_key = "LockID"
-    
+    
+    attribute {
+        name = "LockID"
+        type = "S"
+    }
+
+}
