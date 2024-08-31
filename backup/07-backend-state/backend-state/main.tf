5b8ced72bba06dba2ff746267b17dddbe6746f26
omonuj <jonahodoh@gmail.com>
2024-08-31 14:37:41 +0100

    Flesh out S3 bucket and DynamoDB table to bootstrap remote state

--- backup/07-backend-state/backend-state/main.tf
+++ backup/07-backend-state/backend-state/main.tf
@@ -43,4 +43,10 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
 }
 
 //Locking - Dynamo DB
-
+
+resource "aws_dynamodb_table" "enterprise_backend_lock" {
+    name = "dev_application_locks"
+    billing_mode = "PAY_PER_REQUEST"
+
+    hash_key = "LockID"
+    
