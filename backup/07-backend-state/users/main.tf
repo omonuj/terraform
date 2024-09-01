b048247879213e595dac43a5e62eeea542bab6ab
omonuj <jonahodoh@gmail.com>
2024-09-01 15:59:26 +0100

    Finish IAM users config wired to the S3 backend

--- backup/07-backend-state/users/main.tf
+++ backup/07-backend-state/users/main.tf
@@ -11,4 +11,9 @@ terraform {
 
 provider "aws" {
     region = "us-east-1"
-   // version = "~> 2.46"
+   // version = "~> 2.46"
+}
+
+resource "aws_iam_user" "my_iam_user" {
+    name = "${terraform.workspace}_my_iam_user_abc"
+}
