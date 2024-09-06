98bf11fb5e16c3a87ae3756b78406c2a451c1b1b
omonuj <jonahodoh@gmail.com>
2024-09-06 12:32:16 +0100

    Polish remote state bootstrap with versioning and locking

--- 07-backend-state/backed-state/main.tf
+++ 07-backend-state/backed-state/main.tf
@@ -53,4 +53,10 @@ resource "aws_dynamodb_table" "enterprise_backend_lock" {
   billing_mode = "PAY_PER_REQUEST"
 
   hash_key = "LockID"
-
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+} 
