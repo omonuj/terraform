46fa8b3985baa54e7c4f0355aab398412da0302d
omonuj <jonahodoh@gmail.com>
2024-09-16 12:08:54 +0100

    Grow count based EC2 scaling in the default VPC

--- backup/09-multiple-ec2-instances/main.tf
+++ backup/09-multiple-ec2-instances/main.tf
@@ -14,4 +14,10 @@ resource "aws_security_group" "http_server_sg" {
 
   ingress {
     from_port   = 80
-    to_port     = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
