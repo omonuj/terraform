96d4beea24062a0521036d5805f198faa99d9f3f
omonuj <jonahodoh@gmail.com>
2024-09-17 11:32:20 +0100

    Flesh out count based EC2 scaling in the default VPC

--- backup/09-multiple-ec2-instances/main.tf
+++ backup/09-multiple-ec2-instances/main.tf
@@ -26,4 +26,9 @@ resource "aws_security_group" "http_server_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  egress {
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
